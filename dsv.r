e79f2e11c83759554af1dcbb982b988f12a88b55
Ben Bradford <bradfordium@gmail.com>
2021-05-30 14:22:17 -0500

    update script for style

--- dsv.r
+++ dsv.r
@@ -1,14 +1,16 @@
 # Process CR1000 data logger outputs and generate DSV and PDays
 # Developed by Ben Bradford, UW Madison, 2019 (bbradford@wisc.edu)
 
+library(tidyverse)
 
-# Function definitions ----------------------------------------------------
+
+
+# Define functions --------------------------------------------------------
 
 # load and parse data files from loggers
-loadDat = function(file, loc) {
-  require(dplyr)
+loadDat <- function(file, loc) {
   
-  # parse file
+  # parse TOA5 file
   headers = read.csv(file, skip = 1, header = F, nrows = 1, as.is = T)
   df = read.csv(file, skip = 4, header = F)
   colnames(df) = headers
@@ -21,10 +23,8 @@ loadDat = function(file, loc) {
     mutate(HiRH = case_when(AvgHrRH >= 95 ~ 1, T ~ 0))
 }
 
-
 # generate dsv from leaf wetness hours and avg temp during high RH hours
 dsv <- function(tavgC, lw) {
-  require(dplyr)
   
   # return 0 if arguments are NA
   if (is.na(tavgC) | is.na(lw)) {
@@ -33,49 +33,47 @@ dsv <- function(tavgC, lw) {
   
   # return dsvs based on temp and leaf wetness
   if (tavgC > 13 & tavgC < 18) {
-    return(case_when(
-      lw <=  6 ~ 0,
-      lw <= 15 ~ 1,
-      lw <= 20 ~ 2,
-      T ~ 3))
+    if (lw <= 6) return(0)
+    if (lw <= 15) return(1)
+    if (lw <= 20) return(2)
+    return(3)
   }
+  
   if (tavgC >= 18 & tavgC < 21) {
-    return(case_when(
-      lw <=  3 ~ 0,
-      lw <=  8 ~ 1,
-      lw <= 15 ~ 2,
-      lw <= 22 ~ 3,
-      T ~ 4))
+    if (lw <= 3) return(0)
+    if (lw <= 8) return(1)
+    if (lw <= 15) return(2)
+    if (lw <= 22) return(3)
+    return(4)
   }
+  
   if (tavgC >= 21 & tavgC < 26) {
-    return(case_when(
-      lw <=  2 ~ 0,
-      lw <=  5 ~ 1,
-      lw <= 12 ~ 2,
-      lw <= 20 ~ 3,
-      T ~ 4))
+    if (lw <= 2) return(0)
+    if (lw <= 5) return(1)
+    if (lw <= 12) return(2)
+    if (lw <= 20) return(3)
+    return(4)
   }
-  if (tavgC >= 26 & tavgC < 29) {
-    return(case_when(
-      lw <=  3 ~ 0,
-      lw <=  8 ~ 1,
-      lw <= 15 ~ 2,
-      lw <= 22 ~ 3,
-      T ~ 4))
+  
+  if (tavgC >= 26) {
+    if (lw <= 3) return(0)
+    if (lw <= 8) return(1)
+    if (lw <= 15) return(2)
+    if (lw <= 22) return(3)
+    return(4)
   }
+  
   return(0)
 }
 
-
 # function generates Farenheit p-days from daily min/max temps
 pday <- function(tminF, tmaxF) {
-  require(dplyr)
   
   # check for NA arguments
-  if (is.na(tminF) | is.na(tmaxF)) {return(0)}
+  if (is.na(tminF) | is.na(tmaxF)) return(0)
   
   # calculate temperature quartiles
-  t = data.frame(
+  t <- tibble(
     t1 = tminF,
     t2 = ((2 * tminF) + tmaxF) / 3,
     t3 = (tminF + (2 * tmaxF)) / 3,
@@ -83,41 +81,37 @@ pday <- function(tminF, tmaxF) {
   )
   
   # calculate unmodified pdays from temperatures
-  tm =
-    mutate_all(t,
-      function(t) {
-        case_when(t < 45 ~ 0,
-          between(t, 45, 70) ~ 10 * (1 - (((t - 70) ^ 2) / 625)),
-          between(t, 70, 85) ~ 10 * (1 - (((t - 70) ^ 2) / 256)),
-          T ~ 0)
-      })
+  tm <- t %>%
+    mutate_all(function(t) {
+      case_when(t < 45 ~ 0,
+        between(t, 45, 70) ~ 10 * (1 - (((t - 70) ^ 2) / 625)),
+        between(t, 70, 85) ~ 10 * (1 - (((t - 70) ^ 2) / 256)),
+        T ~ 0)
+    })
   
   # weight and sum pdays
-  Fpday = with(tm, (1 / 24) * ((5 * t1) + (8 * t2) + (8 * t3) + (3 * t4)))
+  Fpday <- with(tm, (1 / 24) * ((5 * t1) + (8 * t2) + (8 * t3) + (3 * t4)))
   
   # return integer pdays
   return(round(Fpday, 2))
 }
 
-
 # convert hourly readings to daily summaries
 makeDaily <- function(df) {
-  require(dplyr)
   
   # average temps from high RH hours
-  HiRH = df %>%
+  HiRH <- df %>%
     filter(HiRH == 1) %>%
     group_by(Date) %>%
-    summarise(TavgC.HiRH = mean(Tair_C_Avg))
+    summarise(TavgC.HiRH = mean(Tair_C_Avg), .groups = "drop")
   
   # main daily summary
-  daily = df %>%
+  daily <- df %>%
     group_by(Year, Location, Date) %>%
     mutate(
       TminF = (Tair_C_Min * 9 / 5) + 32,
       TmaxF = (Tair_C_Max * 9 / 5) + 32,
-      TavgF = (Tair_C_Avg * 9 / 5) + 32
-    ) %>%
+      TavgF = (Tair_C_Avg * 9 / 5) + 32) %>%
     summarise(
       TminC = min(Tair_C_Min),
       TmaxC = max(Tair_C_Max),
@@ -129,68 +123,31 @@ makeDaily <- function(df) {
       MaxRH = max(AvgHrRH),
       MeanRH = mean(AvgHrRH),
       HrsHiRH = sum(HiRH),
-      PrecipIn = sum(Rain_in_Tot)
-    ) %>%
+      PrecipIn = sum(Rain_in_Tot),
+      .groups = "drop") %>%
     group_by(Year) %>%
     mutate(PrecipCumIn = cumsum(PrecipIn))
   
-  joined = daily %>%
+  joined <- daily %>%
     left_join(HiRH) %>%
     group_by(Year) %>%
-    mutate(DSV = mapply(dsv, .$TavgC.HiRH, .$HrsHiRH)) %>%
-    mutate(DSVcum = cumsum(DSV)) %>%
-    mutate(Pday = mapply(pday, .$TminF, .$TmaxF)) %>%
+    mutate(
+      DSV = mapply(dsv, .$TavgC.HiRH, .$HrsHiRH),
+      DSVcum = cumsum(DSV),
+      Pday = mapply(pday, .$TminF, .$TmaxF)) %>%
     mutate(Pdaycum = cumsum(Pday)) %>%
     ungroup() %>%
-    mutate_if(is.numeric, round, 2) %>%
-    select(-"TavgC.HiRH")
+    mutate_if(is.numeric, round, 2)
   
   # replace NA with zero
-  joined[is.na(joined)] = 0
+  joined[is.na(joined)] <- 0
   
   return(joined)
 }
 
 
-# Process station data ----------------------------------------------------
 
-gs <- "1cxdccapGiGpp8w2U4ZwlAH_oiwdLvhfniUtHuBhj75w" # target google sheet
-dateCutoff <- "2020-05-01" # filter values before this date
-year = "2020"
-
-
-runDat <- function(arg, name, remote) {
-  local = paste0(arg, ".dat")
-  file.copy(remote, local, overwrite = T)
-  hourly = loadDat(local, name) %>% filter(Date >= dateCutoff)
-  daily = makeDaily(hourly)
-  write_csv(hourly, paste0(arg, "-hourly-", year, ".csv"))
-  write_csv(daily, paste0(arg, "-", year, ".csv"))
-  write_sheet(daily, gs, arg)
-  logtext(paste("Script completed for station", arg))
-}
-
-
-if (args[1] == "han") {
-  runDat("han", "Hancock", "C:/Campbellsci/LoggerNet/Hancock_Hr1.dat")
-} else if (args[1] == "gma") {
-  runDat("gma", "Grand Marsh", "C:/Campbellsci/LoggerNet/GrandMarsh_Hr1.dat")
-} else if (args[1] == "plo") {
-  runDat("plo", "Plover", "C:/Campbellsci/LoggerNet/Plover_Hr1.dat")
-} else if (args[1] == "ant") {
-  runDat("ant", "Antigo", "C:/Campbellsci/LoggerNet/Antigo_Hr1.dat")
-} else {
-  message("'", args[1], "' is not a valid station name.")
-  logtext("ERROR: Invalid station name.")
-  Sys.sleep(3)
-}
-
-
-
-
-
-
-# Old data ---------------------------------------------------------------
+# process dat files -------------------------------------------------------
 
 gma_old <- loadDat("old_data/GrandMarsh_Hr1.dat", "Grand Marsh")
 han_old <- loadDat("old_data/Hancock_Hr1.dat", "Hancock")
@@ -207,3 +164,25 @@ han_old %>%
 plo_old %>%
   group_by(Year) %>%
   do(write_csv(makeDaily(.), paste0("old_data/Plover_", .$Year[1], ".csv")))
+
+
+
+
+han_old %>%
+  group_by(Year) %>%
+  do(write_csv(makeDaily(.), paste0("test_data/Hancock_", .$Year[1], ".csv")))
+
+
+han_2019 <- filter(han_old, Year == 2019)
+
+han_2019_daily <- makeDaily(han_2019)
+
+han_2020 <- loadDat("han.dat", "Hancock")
+han_2020_daily <- makeDaily(han_2020)
+write_csv(han_2020_daily, "han-2020.csv")
+
+gma_2020 <- loadDat("gma.dat", "Grand Marsh")
+write_csv(makeDaily(gma_2020), "gma-2020.csv")
+
+plo_2020 <- loadDat("plo.dat", "Plover")
+write_csv(makeDaily(plo_2020), "plo-2020.csv")
