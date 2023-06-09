bf46393ecfcbaf8cb219eaac58318860475b8ddd
Ben Bradford <bradfordium@gmail.com>
2023-06-08 22:13:17 -0500

    revamp for 2023. Column names now match AgWeather for simple plotting on VegPath.

--- dsv.r
+++ dsv.r
@@ -1,73 +1,71 @@
 # Process CR1000 data logger outputs and generate DSV and PDays
 # Developed by Ben Bradford, UW Madison, 2019 (bbradford@wisc.edu)
 
-library(tidyverse)
+if (!require(dplyr)) install.packages("dplyr")
+if (!require(readr)) install.packages("readr")
+if (!require(lubridate)) install.packages("lubridate")
+if (!require(googlesheets4)) install.packages("googlesheets4")
 
-
-library(googlesheets4)
-gs4_auth()
+# Must authenticate if no cached oauth token exists
+# gs4_auth()
 
 
 # Define functions --------------------------------------------------------
 
-# load and parse data files from loggers
-loadDat <- function(file, loc) {
-  
-  # parse TOA5 file
-  headers = read.csv(file, skip = 1, header = F, nrows = 1, as.is = T)
-  df = read.csv(file, skip = 4, header = F)
-  colnames(df) = headers
-  
-  # add some columns
-  df %>%
-    mutate(Location = loc) %>%
-    mutate(Date = as.Date(TIMESTAMP)) %>%
-    mutate(Year = as.numeric(format(Date, "%Y"))) %>%
-    mutate(HiRH = case_when(AvgHrRH >= 95 ~ 1, T ~ 0))
+c_to_f <- function(x) {
+  x * 9 / 5 + 32
 }
 
+in_to_mm <- function(x) {
+  x * 25.4
+}
+
+# debugging
+logtext <- function(msg, logfile = "log.txt") {
+  message(msg)
+  cat(format(Sys.time(), "\n[%Y-%m-%d %X] "), msg, "\n", file = logfile, append = T)
+}
+
+
 # generate dsv from leaf wetness hours and avg temp during high RH hours
-dsv <- function(tavgC, lw) {
+# leaf wetness (hours high RH) lw
+# temp t in Celsius
+dsv <- function(t, lw) {
   
   # return 0 if arguments are NA
-  if (is.na(tavgC) | is.na(lw)) {
+  if (is.na(t) | is.na(lw)) {
     return(0)
   }
   
   # return dsvs based on temp and leaf wetness
-  if (tavgC > 13 & tavgC < 18) {
-    if (lw <= 6) return(0)
-    if (lw <= 15) return(1)
-    if (lw <= 20) return(2)
+  if (t < 7.2) return(0)
+  
+  if (t <= 11.6) {
+    if (lw <= 16) return(0)
+    if (lw <= 18) return(1)
+    if (lw <= 21) return(2)
     return(3)
   }
   
-  if (tavgC >= 18 & tavgC < 21) {
-    if (lw <= 3) return(0)
-    if (lw <= 8) return(1)
-    if (lw <= 15) return(2)
-    if (lw <= 22) return(3)
+  if (t <= 15) {
+    if (lw <= 13) return(0)
+    if (lw <= 15) return(1)
+    if (lw <= 18) return(2)
+    if (lw <= 21) return(3)
     return(4)
   }
   
-  if (tavgC >= 21 & tavgC < 26) {
-    if (lw <= 2) return(0)
-    if (lw <= 5) return(1)
-    if (lw <= 12) return(2)
-    if (lw <= 20) return(3)
-    return(4)
-  }
-  
-  if (tavgC >= 26) {
-    if (lw <= 3) return(0)
-    if (lw <= 8) return(1)
+  if (t <= 26.6) {
+    if (lw <= 10) return(0)
+    if (lw <= 12) return(1)
     if (lw <= 15) return(2)
-    if (lw <= 22) return(3)
+    if (lw <= 18) return(3)
     return(4)
   }
   
   return(0)
 }
+
 
 # function generates Farenheit p-days from daily min/max temps
 pday <- function(tminF, tmaxF) {
@@ -99,145 +97,177 @@ pday <- function(tminF, tmaxF) {
   return(round(Fpday, 2))
 }
 
+
+# load and parse data files from loggers
+loadDat <- function(file, loc, yr = format(Sys.Date(), "%Y")) {
+  
+  # parse TOA5 file
+  headers = read.csv(file, skip = 1, header = F, nrows = 1, as.is = T)
+  df = read.csv(file, skip = 4, header = F)
+  colnames(df) = headers
+  
+  # format input data
+  df %>%
+    as_tibble() %>%
+    select(-RECORD) %>%
+    mutate(
+      Location = loc,
+      Date = as.Date(TIMESTAMP),
+      Year = year(Date),
+      DayOfYear = yday(Date)
+    ) %>%
+    filter(Year == yr) %>%
+    select(
+      Location,
+      Timestamp = TIMESTAMP,
+      Date,
+      Year,
+      DOY = DayOfYear,
+      HourOfDay,
+      MinTempC = Tair_C_Min,
+      MaxTempC = Tair_C_Max,
+      AvgTempC = Tair_C_Avg,
+      MinSoilTempC = Tsoil_C_Min,
+      MaxSoilTempC = Tsoil_C_Max,
+      AvgSoilTempC = Tsoil_C_Avg,
+      PrecipIn = Rain_in_Tot,
+      AvgHrRH,
+      everything()
+    ) %>%
+    mutate(
+      MinTempF = c_to_f(MinTempC),
+      MaxTempF = c_to_f(MaxTempC),
+      AvgTempF = c_to_f(AvgTempC),
+      .after = AvgTempC
+    ) %>%
+    mutate(
+      MinSoilTempF = c_to_f(MinSoilTempC),
+      MaxSoilTempF = c_to_f(MaxSoilTempC),
+      AvgSoilTempF = c_to_f(AvgSoilTempC),
+      .after = AvgSoilTempC
+    ) %>%
+    mutate(
+      PrecipMm = in_to_mm(PrecipIn), .after = PrecipIn
+    ) %>%
+    mutate(HiRH = ifelse(AvgHrRH >= 95, 1, 0), .after = AvgHrRH) %>%
+    mutate(across(
+      where(is.numeric),  ~ case_when(
+        is.nan(.x) ~ NA,
+        .x < 0 ~ NA,
+        T ~ .x
+      ))
+    ) %>%
+    mutate(Record = row_number() - 1, .before = everything())
+}
+
+
+
 # convert hourly readings to daily summaries
-makeDaily <- function(df) {
+makeDaily <- function(hourly) {
   
   # average temps from high RH hours
-  HiRH <- df %>%
+  HiRH <- hourly %>%
     filter(HiRH == 1) %>%
     group_by(Date) %>%
-    summarise(TavgC.HiRH = mean(Tair_C_Avg), .groups = "drop")
+    summarise(AvgTempC_HiRH = mean(AvgTempC), .groups = "drop")
   
   # main daily summary
-  daily <- df %>%
+  daily <- hourly %>%
     group_by(Year, Location, Date) %>%
-    mutate(
-      TminF = (Tair_C_Min * 9 / 5) + 32,
-      TmaxF = (Tair_C_Max * 9 / 5) + 32,
-      TavgF = (Tair_C_Avg * 9 / 5) + 32) %>%
     summarise(
-      TminC = min(Tair_C_Min),
-      TmaxC = max(Tair_C_Max),
-      TavgC = mean(Tair_C_Avg),
-      TminF = min(TminF),
-      TmaxF = max(TmaxF),
-      TavgF = mean(TavgF),
+      MinTempC = min(MinTempC),
+      MaxTempC = max(MaxTempC),
+      AvgTempC = mean(AvgTempC),
+      MinTempF = min(MinTempF),
+      MaxTempF = max(MaxTempF),
+      AvgTempF = mean(AvgTempF),
+      AvgSoilTempC = mean(AvgSoilTempC),
+      AvgSoilTempF = mean(AvgSoilTempF),
       MinRH = min(AvgHrRH),
       MaxRH = max(AvgHrRH),
-      MeanRH = mean(AvgHrRH),
+      AvgRH = mean(AvgHrRH),
       HrsHiRH = sum(HiRH),
-      PrecipIn = sum(Rain_in_Tot),
+      PrecipIn = sum(PrecipIn),
+      PrecipMm = sum(PrecipMm),
       .groups = "drop") %>%
     group_by(Year) %>%
-    mutate(PrecipCumIn = cumsum(PrecipIn))
+    mutate(
+      CumPrecipIn = cumsum(PrecipIn),
+      CumPrecipMm = cumsum(PrecipMm)
+    )
   
   joined <- daily %>%
-    left_join(HiRH) %>%
+    left_join(HiRH, by = "Date") %>%
     group_by(Year) %>%
     mutate(
-      DSV = mapply(dsv, .$TavgC.HiRH, .$HrsHiRH),
-      DSVcum = cumsum(DSV),
-      Pday = mapply(pday, .$TminF, .$TmaxF)) %>%
-    mutate(Pdaycum = cumsum(Pday)) %>%
+      DSV = mapply(dsv, .$AvgTempC_HiRH, .$HrsHiRH),
+      CumDSV = cumsum(DSV),
+      PDay = mapply(pday, .$MinTempF, .$MaxTempF),
+      CumPDay = cumsum(PDay)
+    ) %>%
+    mutate() %>%
     ungroup() %>%
-    mutate_if(is.numeric, round, 2)
+    mutate_if(is.numeric, round, 2) %>%
+    mutate(Record = row_number() - 1, .before = everything())
   
-  # replace NA with zero
-  joined[is.na(joined)] <- 0
+  # Pyranometer data
+  if ("SlrMJm2_Tot" %in% names(hourly)) {
+    insol <- hourly %>%
+      group_by(Date) %>%
+      summarise(SlrMJm2_Tot = round(sum(SlrMJm2_Tot), 2))
+    joined <- joined %>%
+      left_join(insol, by = "Date")
+  }
   
   return(joined)
 }
 
-
-
-# process dat files -------------------------------------------------------
-
-gma_old <- loadDat("old_data/GrandMarsh_Hr1.dat", "Grand Marsh")
-han_old <- loadDat("old_data/Hancock_Hr1.dat", "Hancock")
-plo_old <- loadDat("old_data/Plover_Hr1.dat", "Plover")
-
-gma_old %>%
-  group_by(Year) %>%
-  do(write_csv(makeDaily(.), paste0("old_data/GrandMarsh_", .$Year[1], ".csv")))
-
-han_old %>%
-  group_by(Year) %>%
-  do(write_csv(makeDaily(.), paste0("old_data/Hancock_", .$Year[1], ".csv")))
-
-plo_old %>%
-  group_by(Year) %>%
-  do(write_csv(makeDaily(.), paste0("old_data/Plover_", .$Year[1], ".csv")))
-
-
-
-
-han_old %>%
-  group_by(Year) %>%
-  do(write_csv(makeDaily(.), paste0("test_data/Hancock_", .$Year[1], ".csv")))
-
-
-han_2019 <- filter(han_old, Year == 2019)
-
-han_2019_daily <- makeDaily(han_2019)
-
-han_2020 <- loadDat("han.dat", "Hancock")
-han_2020_daily <- makeDaily(han_2020)
-write_csv(han_2020_daily, "han-2020.csv")
-
-gma_2020 <- loadDat("gma.dat", "Grand Marsh")
-write_csv(makeDaily(gma_2020), "gma-2020.csv")
-
-plo_2020 <- loadDat("plo.dat", "Plover")
-write_csv(makeDaily(plo_2020), "plo-2020.csv")
-
-
-
-# 2022 --------------------------------------------------------------------
-
-file = "han.dat"
-loc = "Hancock"
-arg = "han"
-
-han_hourly <- loadDat("han.dat", "Hancock")
-write_csv(hourly, paste0(arg, "-hourly-", year, ".csv"))
-write_sheet(daily, gs_hourly, arg)
-
-
-han_daily = makeDaily(han_hourly)
-
+# Main process
+runDat <- function(arg, name, remote) {
+  local <- paste0(arg, ".dat")
   
-han_hourly %>%
-  mutate(across(contains("_C_"), c_to_f, .names = "{.col}_F")) %>%
-  rename_with(~ gsub("_C", "", .x), contains("_F")) %>%
-  view()
-
-
-c_to_f <- function(temp) {
-  temp * 9 / 5.0 + 32
+  # copy dat to working directory
+  file.copy(remote, local, overwrite = T)
+  
+  # process data
+  hourly <- loadDat(local, name) %>%
+    filter(Date >= dateCutoff)
+  daily <- makeDaily(hourly)
+  
+  # save locally to csv
+  write_csv(hourly, paste0(arg, "-hourly-", year, ".csv"))
+  write_csv(daily, paste0(arg, "-", year, ".csv"))
+  
+  # upload to google sheets
+  write_sheet(daily, gs_daily, name)
+  write_sheet(hourly, gs_hourly, name)
+  
+  logtext(paste("Script completed for", name))
 }
 
-hourly <- loadDat("han.dat", "Hancock")
-write_csv(hourly, paste0(arg, "-hourly-", year, ".csv"))
-write_sheet(hourly, gs_hourly, arg)
 
+# Define variables ----
 
-daily <- makeDaily(hourly)
-write_csv(daily, paste0(arg, "-", year, ".csv"))
-write_sheet(daily, gs_daily, arg)
+# google sheets targets
+gs_daily <- "1sKiKiZzLbW0C2y0piXrkJkxYlLYq2GW_DC45tvdinzk"
+gs_hourly <- "1YvizlFmpkq5rVYhzuAylTmUQQRKPyAS3thBW8OCxOhs"
 
+# filter values before this date
+year <- strftime(Sys.Date(), "%Y")
+dateCutoff <- paste0(year, "-05-01")
 
-hourly
-gsub("_C_", "_F_", names(hourly))
-
-hourly %>%
-  rename_with(~ paste0(gsub("_C", "", .x), "_C"), .cols = contains("_C_"))
-
-
-
-han_daily %>%
-  mutate(across(where(is.numeric), ~ replace(.x, !is.finite(.x), NA)))
-
-~replace(is.finite(.x), .x, NA)
-
-han_daily[han_daily == Inf]
+# valid stations
+stn_codes <- c("han", "gma", "plo", "ant")
+stn_names <- list(
+  "han" = "Hancock",
+  "gma" = "Grand Marsh",
+  "plo" = "Plover",
+  "ant" = "Antigo"
+)
+stn_files <- list(
+  "han" = "C:/Campbellsci/LoggerNet/Hancock_Hourly.dat",
+  "gma" = "C:/Campbellsci/LoggerNet/GrandMarsh_Hourly.dat",
+  "plo" = "C:/Campbellsci/LoggerNet/Plover_Hourly.dat",
+  "ant" = "C:/Campbellsci/LoggerNet/Antigo_Hourly.dat"
+)
 
